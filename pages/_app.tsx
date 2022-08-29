94709d4ac19d190fc6032186568315640e9de1ce
beka <badagadzeb@gmail.com>
2022-08-29 14:46:47 +0400

    add messanger

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import Head from "next/head";
 import "../style/style.scss";
 import "react-toastify/dist/ReactToastify.css";
 import { SearchProvider } from "../components/pages/search/context/searchContext";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+
 // import { Html } from "next/document";
 
 function MyApp({ Component, pageProps }) {
@@ -17,6 +19,10 @@ function MyApp({ Component, pageProps }) {
         {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> */}
       </Head>
       <div>
+        <MessengerCustomerChat
+          pageId="103648229133196"
+          appId="1116841412243862"
+        />
         <SearchProvider>
           <Provider store={store}>
             <Component {...pageProps} />
