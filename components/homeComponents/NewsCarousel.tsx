c004300f0c8267f1dd6cb8a1ffbad923aac766fc
Lado Asambadze <lado.asambadze1@gmail.com>
2024-02-12 13:23:58 +0400

    create newscarousel item with mobile and desktop resolutions

--- components/homeComponents/NewsCarousel.tsx
+++ components/homeComponents/NewsCarousel.tsx
@@ -9,8 +9,10 @@ import {
 } from "../../@/components/ui/carousel";
 import { Card, CardContent } from "../../@/components/ui/card";
 import CarouselBgMen from "../../public/newImages/CarouselBgMen.svg";
+import CarouselBgWomen from "../../public/newImages/CarouselBgWomen.svg";
 import Image from "next/image";
 import { Button } from "../../@/components/ui/button";
+import { useMediaQuery } from "react-responsive";
 
 const data = [
   {
@@ -22,7 +24,7 @@ const data = [
   {
     header: "სათაური",
     text: "შემთხვევითად გენერირებული ტექსტი ეხმარება დიზაინერებს და ტიპოგრაფიული ნაწარმის",
-    image: CarouselBgMen,
+    image: CarouselBgWomen,
     buttonText: "ღილაკი",
   },
   {
@@ -34,45 +36,45 @@ const data = [
 ];
 
 export default function NewsCarousel() {
+  const media = useMediaQuery({ query: "(max-width: 768px)" });
   return (
-    <main className="flex flex-col items-start w-full my-12">
+    <main className="w-full flex flex-col items-start my-12 md:my-24">
       <h1 className="text-2xl text-[#484848]">სათაური</h1>
       <h2 className="mt-2 text-xs text-[#484848]">
         შემთხვევით გენერირებული ტექსტი
       </h2>
-      <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full mt-6 "
-      >
-        <CarouselContent>
-          {data.map((item, index) => (
-            <CarouselItem
-              key={index}
-              className="md:basis-1/2  lg:basis-1/2 xl:basis-1/2 "
-            >
-              <div className="p-1 ">
-                <Card className="rounded-xl pt-6 pl-6 pb-10 pr-28  bg-[#c0dbfc] w-[500px] h-[240px]  relative md:pt-8 md:pl-8 xl:pr-[228px]  ">
-                  <CardContent className="p-0 flex flex-col relative ">
-                    <span className="text-base font-semibold">
-                      {item.header}
-                    </span>
-                    <span className="text-xs pt-4">{item.text}</span>
+      <main className="w-full  md:px-24 ">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full mt-6 p-0 "
+        >
+          <CarouselContent className="pr-10 md:pr-16">
+            {data.map((item, index) => (
+              <CarouselItem key={index} className="w-full md:basis-1/2   ">
+                <Card className="w-full rounded-xl bg-[#c0dbfc] ">
+                  <CardContent className="w-full p-0   rounded-xl">
+                    <div className="w-full pl-6 pt-6 pb-10 pr-24 flex flex-col relative md:pb-10 md:pt-8 md:pl-8 md:pr-[226px] ">
+                      <span className="text-base font-semibold">
+                        {item.header}
+                      </span>
+                      <span className="text-xs ">{item.text}</span>
 
-                    <Button className="mt-6">{item.buttonText}</Button>
-                    <div className="absolute right-[-111px] bottom-[-46px] md:right-[-228px] ">
-                      <Image width={128} height={158} src={item.image} />
+                      <Button className="mt-6 w-32">{item.buttonText}</Button>
+                      <div className="absolute right-0 bottom-[-10px]  md:right-4">
+                        <Image width={128} height={158} src={item.image} />
+                      </div>
                     </div>
                   </CardContent>
                 </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {media ? null : <CarouselPrevious />}
+          {media ? null : <CarouselNext />}
+        </Carousel>
+      </main>
     </main>
   );
 }
