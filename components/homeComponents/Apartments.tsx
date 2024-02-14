569bfee45a230e1f0bbd8df46c7fff3f73b74fb1
Lado Asambadze <lado.asambadze1@gmail.com>
2024-02-14 15:46:05 +0400

    add conditional check for flats before mapping

--- components/homeComponents/Apartments.tsx
+++ components/homeComponents/Apartments.tsx
@@ -27,38 +27,39 @@ export default function Apartments({ flats }) {
           className="w-full p-0   "
         >
           <CarouselContent className=" pr-10 lg:pr-16">
-            {flats.map((item) => (
-              <Link href={`house/${item.id}`}>
-                <CarouselItem
-                  key={item}
-                  className="w-full sm:basis-1/2  md:basis-1/3 xl:basis-1/4 pointer"
-                >
-                  <div className="w-full  flex flex-col justify-start rounded-xl  items-start  border-1 ">
-                    <div className="w-full  rounded-t-xl overflow-hidden">
-                      <div style={{ height: "214px", position: "relative" }}>
-                        <Image
-                          src={item.images[0].original}
-                          layout="fill"
-                          objectFit="cover"
-                        />
+            {flats &&
+              flats.map((item) => (
+                <Link href={`house/${item.id}`}>
+                  <CarouselItem
+                    key={item}
+                    className="w-full sm:basis-1/2  md:basis-1/3 xl:basis-1/4 pointer"
+                  >
+                    <div className="w-full  flex flex-col justify-start rounded-xl  items-start  border-1 ">
+                      <div className="w-full  rounded-t-xl overflow-hidden">
+                        <div style={{ height: "214px", position: "relative" }}>
+                          <Image
+                            src={item.images[0].original}
+                            layout="fill"
+                            objectFit="cover"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="pl-4  pt-7 pb-4">
+                        <h1 className="text-semibold text-xl text-[#484848]">
+                          2987 ₾/თვეში
+                        </h1>
+                        <p className="mt-3 text-xs text-[#484848]">
+                          3 საძინებელი, ფართი - 85 მ 2
+                        </p>
+                        <p className="mt-1 text-xs text-[#484848]">
+                          საბურთალო: მერაბ კოსტავა #34
+                        </p>
                       </div>
                     </div>
-
-                    <div className="pl-4  pt-7 pb-4">
-                      <h1 className="text-semibold text-xl text-[#484848]">
-                        2987 ₾/თვეში
-                      </h1>
-                      <p className="mt-3 text-xs text-[#484848]">
-                        3 საძინებელი, ფართი - 85 მ 2
-                      </p>
-                      <p className="mt-1 text-xs text-[#484848]">
-                        საბურთალო: მერაბ კოსტავა #34
-                      </p>
-                    </div>
-                  </div>
-                </CarouselItem>
-              </Link>
-            ))}
+                  </CarouselItem>
+                </Link>
+              ))}
           </CarouselContent>
 
           {media ? null : <CarouselPrevious />}
