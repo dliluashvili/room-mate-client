83daffead17bd8393351b0c6cd790d2ece46401f
Lado Asambadze <lado.asambadze1@gmail.com>
2024-02-14 15:34:20 +0400

    add image domain for test domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextTranslate = require("next-translate");
 
 module.exports = {
   images: {
-    domains: ["img.roommategeorgia.ge"],
+    domains: ["img.roommategeorgia.ge", "img.test.roommategeorgia.ge"],
   },
   eslint: {
     ignoreDuringBuilds: true,
