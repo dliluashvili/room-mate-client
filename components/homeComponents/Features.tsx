1979ac18c8610e4fc60a84d60d4cbd50d1d32673
Lado Asambadze <lado.asambadze1@gmail.com>
2024-02-29 14:28:50 +0400

    update styles for features section of home page

--- components/homeComponents/Features.tsx
+++ components/homeComponents/Features.tsx
@@ -11,13 +11,15 @@ export default function Features() {
   let { t } = useTranslation("common") as { t: (key: string) => string };
   return (
     <>
-      <main className="w-full flex justify-between px-6  mt-12 sm:px-16 md:px-20 lg:grid  lg:grid-flow-col  lg:grid-cols-4 lg:gap-x-20 lg:mt-24 lg:px-24">
+      <main className="w-full flex justify-between px-6  mt-12 sm:px-16 md:px-20 lg:grid  lg:grid-flow-col  lg:grid-cols-4 lg:gap-x-12 lg:mt-24 lg:px-24">
         <div className="flex flex-col items-center w-16 lg:w-full lg:items-start">
           <Image src={BankIcon} alt="Bank Icon" width={32} height={32} />
           <div className="flex flex-col mt-4">
-            <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
-              {t("roommateFind")}
-            </span>
+            <div className=" h-12 md:h-20  xl:h-10 w-full flex items-start">
+              <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
+                {t("roommateFind")}
+              </span>
+            </div>
             <p className="hidden lg:block text-[#484848] text-base font-normal mt-2">
               {t("roommateFindText")}
             </p>
@@ -26,9 +28,11 @@ export default function Features() {
         <div className="flex flex-col items-center w-16 lg:w-full lg:items-start">
           <Image src={BankIcon} alt="Bank Icon" width={32} height={32} />
           <div className="flex flex-col mt-4">
-            <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
-              {t("roommateMatching")}
-            </span>
+            <div className=" h-12  md:h-20 xl:h-10 w-full flex items-start">
+              <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
+                {t("roommateMatching")}
+              </span>
+            </div>
             <p className="hidden lg:block text-[#484848] text-base mt-2">
               {t("roommateMatchingText")}
             </p>
@@ -37,9 +41,11 @@ export default function Features() {
         <div className="flex flex-col items-center w-16 lg:w-full lg:items-start">
           <Image src={BankIcon} alt="Bank Icon" width={32} height={32} />
           <div className="flex flex-col mt-4">
-            <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
-              {t("support")}
-            </span>
+            <div className=" h-12 md:h-20  xl:h-10 w-full flex items-start">
+              <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
+                {t("support")}
+              </span>
+            </div>
             <p className="hidden lg:block text-[#484848] text-base mt-2">
               {t("supportText")}
             </p>
@@ -61,9 +67,11 @@ export default function Features() {
             />
           </div>
           <div className="flex flex-col mt-4">
-            <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
-              4.9
-            </span>
+            <div className=" h-12  md:h-20 xl:h-10 w-full flex items-start">
+              <span className="text-xs break-words text-center text-[#838cac] lg:text-[#484848] lg:text-lg lg:text-left lg:font-semibold">
+                4.9
+              </span>
+            </div>
             <p className="hidden lg:block text-[#484848] text-base mt-2">
               {t("rate")}
             </p>
