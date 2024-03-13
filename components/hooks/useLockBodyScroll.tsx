ec3a31fbabd898b738099148e2059ec781cb2f6d
Lado Asambadze <lado.asambadze1@gmail.com>
2024-03-13 18:13:55 +0400

    fix user fixed filters and use bodylock func by statemnts

--- components/hooks/useLockBodyScroll.tsx
+++ components/hooks/useLockBodyScroll.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-export function useLockBodyScroll() {
+export function useLockBodyScroll(lock) {
   React.useLayoutEffect(() => {
+    // Store original overflow
     const originalStyle = window.getComputedStyle(document.body).overflow;
-    document.body.style.overflow = "hidden";
+
+    // Conditionally lock or unlock body scroll
+    if (lock) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = originalStyle;
+    }
+
+    // Cleanup function
     return () => {
       document.body.style.overflow = originalStyle;
     };
-  }, []);
+  }, [lock]); // Add 'lock' to the dependency array
 }
