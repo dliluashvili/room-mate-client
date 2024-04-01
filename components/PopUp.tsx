1a69a8213a96d729284d7f6a6c1591c58cf63034
Lado Asambadze <lado.asambadze1@gmail.com>
2024-04-01 14:42:01 +0400

    update/modify popup window with new text and nationality logic

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -5,27 +5,27 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
 
-export function PopUp({ isOpen, setIsOpen, range }) {
+export function PopUp({ isOpen, setIsOpen, range, country }) {
   const router = useRouter();
   let { t } = useTranslation("common") as { t: (key: string) => string };
 
   return (
-    <Dialog open={isOpen}>
+    <Dialog open={isOpen && country !== "145"}>
       <h1 onClick={() => setIsOpen(false)}>Close</h1>
       <DialogContent className="sm:max-w-[470px] flex flex-col   overflow-hidden bg-white ">
         <div className="w-full h-40 relative">
           <Image src={popupBack} layout="fill" objectFit="cover" />
         </div>
-        <div className="flex flex-col  gap-2 px-2 md:px-6 text-center pb-2 md:pb-4 text-sm">
+        <div className="flex flex-col  gap-4 px-2 md:px-6 text-center pb-2 md:pb-4 text-sm">
           <span>
             {range < 135
               ? t("135-1")
               : range > 135 && range < 270
               ? t("270-1")
-              : range > 270 && range < 380
-              ? t("380-1")
-              : range > 435
+              : range > 270 && range < 435
               ? t("435-1")
+              : range > 435
+              ? t("500-1")
               : ""}
           </span>
           <span>
@@ -33,10 +33,10 @@ export function PopUp({ isOpen, setIsOpen, range }) {
               ? t("135-2")
               : range > 135 && range < 270
               ? t("270-2")
-              : range > 270 && range < 380
-              ? t("380-2")
-              : range > 435
+              : range > 270 && range < 435
               ? t("435-2")
+              : range > 435
+              ? t("500-2")
               : ""}
           </span>
           <span>
@@ -44,75 +44,64 @@ export function PopUp({ isOpen, setIsOpen, range }) {
               ? t("135-3")
               : range > 135 && range < 270
               ? t("270-3")
-              : range > 270 && range < 380
-              ? t("380-3")
-              : range > 435
+              : range > 270 && range < 435
               ? t("435-3")
+              : range > 435
+              ? t("500-3")
               : ""}
-          </span>
-          <span>
+            <br />
             {range < 135
               ? t("135-4")
               : range > 135 && range < 270
               ? t("270-4")
-              : range > 270 && range < 380
-              ? t("380-4")
-              : range > 435
-              ? t("435-4")
               : ""}
-            <br />
+          </span>
+          <span>
             {range < 135
               ? t("135-5")
               : range > 135 && range < 270
               ? t("270-5")
-              : range > 270 && range < 380
-              ? t("380-5")
+              : range > 270 && range < 435
+              ? t("435-4")
               : range > 435
-              ? t("435-5")
+              ? t("500-4")
               : ""}
           </span>
           <span>
             {range < 135
               ? t("135-6")
               : range > 135 && range < 270
-              ? t("270-6")
-              : range > 270 && range < 380
-              ? t("380-6")
+              ? ""
+              : range > 270 && range < 435
+              ? t("435-5")
               : range > 435
-              ? t("435-6")
+              ? t("500-5")
               : ""}
+            <br />
+            {range > 270 && range < 435 ? t("435-6") : ""}
+            {range > 435 ? t("500-6") : ""}
+            <br />
+            {range > 270 && range < 435 ? t("435-7") : ""}
+            {range > 435 ? t("500-7") : ""}
           </span>
 
-          <span>
-            {range < 135
-              ? t("135-7")
-              : range > 135 && range < 270
-              ? ""
-              : range > 270 && range < 380
-              ? t("380-7")
-              : range > 435
-              ? t("435-7")
-              : ""}
-          </span>
           <div className="flex flex-row gap-2 mt-2 ">
-            <Button variant="popUp" onClick={() => router.push("/")}>
-              {range < 135
-                ? t("135-no")
-                : range > 135 && range < 270
-                ? t("270-no")
-                : range > 270 && range < 380
-                ? t("380-no")
-                : range > 435
-                ? t("435-no")
-                : ""}
-            </Button>
+            {range < 135 ? (
+              <Button variant="popUp" onClick={() => router.push("/")}>
+                {t("135-no")}
+              </Button>
+            ) : range > 135 && range < 270 ? (
+              <Button variant="popUp" onClick={() => router.push("/")}>
+                {t("270-no")}
+              </Button>
+            ) : null}
             <Button variant="popUp" onClick={() => router.push("/")}>
               {range < 135
                 ? t("135-yes")
                 : range > 135 && range < 270
                 ? t("270-yes")
-                : range > 270 && range < 380
-                ? t("380-yes")
+                : range > 270 && range < 435
+                ? t("435-yes")
                 : range > 435
                 ? t("435-yes")
                 : ""}
