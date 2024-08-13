fd9e0fd434140cb414cf3632aabf648f627b9cca
Lado Asambadze <lado.asambadze1@gmail.com>
2024-08-13 14:22:05 +0400

    update gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import { ApolloWrapper } from "../providers/apolloProvider";
 import TwilioClientWrapper from "../components/TwilioClientWrapper";
 import { Toaster } from "../@/components/ui/toaster";
 import Facebook from "../components/tracking/Facebook";
+import Script from "next/script";
 
 // import { Html } from "next/document";
 
@@ -57,10 +58,6 @@ function MyApp({ Component, pageProps }) {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=G-90LQL896FN`}
         />
-        <script
-          async
-          src={`https://www.googletagmanager.com/ns.html?id=GTM-586WWRZ6`}
-        />
 
         <script
           dangerouslySetInnerHTML={{
@@ -74,21 +71,25 @@ function MyApp({ Component, pageProps }) {
           }}
         />
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-586WWRZ6')
-          `,
-          }}
-        />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-586WWRZ6}');
+        `}
+        </Script>
 
         {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> */}
       </Head>
       <div>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-586WWRZ6" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+
         <Facebook />
         <MessengerCustomerChat
           pageId="103648229133196"
