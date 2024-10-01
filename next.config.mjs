22b34a67979fcb956aecbe97ef676d7c232ab002
Lado Asambadze <lado.asambadze1@gmail.com>
2024-10-01 13:15:06 +0400

    fix cross origin issue

--- next.config.mjs
+++ next.config.mjs
@@ -37,23 +37,23 @@ const nextConfig = {
         ],
     },
     // Add Partytown headers
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cross-Origin-Opener-Policy',
-                        value: 'same-origin',
-                    },
-                    {
-                        key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
-                    },
-                ],
-            },
-        ]
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/(.*)',
+    //             headers: [
+    //                 {
+    //                     key: 'Cross-Origin-Opener-Policy',
+    //                     value: 'same-origin',
+    //                 },
+    //                 {
+    //                     key: 'Cross-Origin-Embedder-Policy',
+    //                     value: 'require-corp',
+    //                 },
+    //             ],
+    //         },
+    //     ]
+    // },
 }
 
 export default nextConfig
