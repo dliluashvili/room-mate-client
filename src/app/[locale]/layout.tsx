46a5f49365dc4989fdcd7a1f7b810c5656ef91cb
Lado Asambadze <lado.asambadze1@gmail.com>
2024-10-02 11:42:51 +0400

    delete partytown

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,8 +10,7 @@ import Footer from '@/src/components/footer/Footer'
 import { ModalWrapper } from './(auth)/_modal/ModalWrapper'
 import { AuthWrapper } from '@/src/auth/authWrapper'
 import TwilioClientWrapper from '@/src/conversation/TwilioClientWrapper'
-import Script from 'next/script'
-import { Partytown } from '@builder.io/partytown/react'
+
 import { GoogleTagManager } from '@next/third-parties/google'
 
 const georgian = Noto_Sans_Georgian({ subsets: ['latin'] })
@@ -66,30 +65,6 @@ export default async function RootLayout({
 
     return (
         <html lang={locale} dir={dir(locale)} className={georgian.className}>
-            <head>
-                <Partytown debug={true} forward={['dataLayer.push', 'hj', 'hj.q']} />
-                <Script id="google-tag-manager" strategy="worker">
-                    {`
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-586WWRZ6');
-                    `}
-                </Script>
-                <Script id="hotjar" strategy="worker">
-                    {`
-                        (function(h,o,t,j,a,r){
-                            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                            h._hjSettings={hjid:3128732,hjsv:6};
-                            a=o.getElementsByTagName('head')[0];
-                            r=o.createElement('script');r.async=1;
-                            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                            a.appendChild(r);
-                        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                    `}
-                </Script>
-            </head>
             <body>
                 <TranslationsProvider
                     namespaces={i18nNamespaces}
